d28c4e2f8a562cbb4c4b707c08f3034bc5781399
ODHIAMBO FELIX <odhiambo3felix@gmail.com>
2025-10-30 12:50:05 +0000

    m=base:336  POST https://play.google.com/log?format=json&hasfast=true&a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-5662153056-1e9f9",
-  "appId": "1:563099434057:web:440d9d6ff76bfe122d1398",
+  "appId": "1:563099434057:web:440d6d6ff76bfe122d1398",
   "apiKey": "AIzaSyBzSctQMnP7dsuj0526x2dv1Qi0Mzg89dQ",
   "authDomain": "studio-5662153056-1e9f9.firebaseapp.com",
-  "measurementId": "",
   "messagingSenderId": "563099434057"
 };
