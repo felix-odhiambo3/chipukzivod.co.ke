d52cdd239dd6c821fe0c8152c80f73dae4a8af9d
ODHIAMBO FELIX <odhiambo3felix@gmail.com>
2025-11-03 07:55:09 +0000

    sig up form should ensure best password format for security, Uppercase,

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -34,7 +34,16 @@ const formSchema = z
   .object({
     displayName: z.string().min(2, 'Name must be at least 2 characters.'),
     email: z.string().email(),
-    password: z.string().min(6, 'Password must be at least 6 characters.'),
+    password: z
+      .string()
+      .min(8, 'Password must be at least 8 characters long.')
+      .regex(/[A-Z]/, 'Password must contain at least one uppercase letter.')
+      .regex(/[a-z]/, 'Password must contain at least one lowercase letter.')
+      .regex(/[0-9]/, 'Password must contain at least one number.')
+      .regex(
+        /[!@#$%^&*(),.?":{}|<>]/,
+        'Password must contain at least one special character.'
+      ),
     confirmPassword: z.string(),
   })
   .refine(data => data.password === data.confirmPassword, {
