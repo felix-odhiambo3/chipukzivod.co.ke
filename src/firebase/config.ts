71d555259ef2ce8cda73482f5e844a80bfd203ef
ODHIAMBO FELIX <odhiambo3felix@gmail.com>
2025-11-03 08:16:36 +0000

    Upload error: FirebaseError: Firebase Storage: No default bucket found.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,6 +2,7 @@
 export const firebaseConfig = {
   "projectId": "studio-5662153056-1e9f9",
   "appId": "1:563099434057:web:440d6d6ff76bfe122d1398",
+  "storageBucket": "studio-5662153056-1e9f9.appspot.com",
   "apiKey": "AIzaSyBzSctQMnP7dsuj0526x2dv1Qi0Mzg89dQ",
   "authDomain": "studio-5662153056-1e9f9.firebaseapp.com",
   "messagingSenderId": "563099434057"
